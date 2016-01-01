e4e9e228e46fb4d639fa966d3aa3a1e8cb6a9f90
Peter LaValle <peter.lavalle@gmail.com>
2016-01-01 17:42:56 +0000

    moves the example to its own folder

---
+++ example-01/scala-js/src/main/scala/peterlavalle/dukscala/DukScala.scala
@@ -0,0 +1,14 @@
+package peterlavalle.dukscala
+
+import scala.scalajs.js.annotation.JSExport
+
+/**
+ * Stoopid .scala program to test how haard it is to use Scala-JS in DukTape
+ */
+@JSExport
+object DukScala {
+	@JSExport
+	def foo(): String = {
+		"Ohhh My!!"
+	}
+}
