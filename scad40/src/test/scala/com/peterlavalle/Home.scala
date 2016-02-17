0d0e9f88195431504bd0cf22b95e8c1e19585614
Peter LaValle <peter.lavalle@gmail.com>
2016-02-17 08:17:27 +0000

    re-jiggle sources
    --HG--
    branch : staging

---
+++ scad40/src/test/scala/com/peterlavalle/Home.scala
@@ -0,0 +1,25 @@
+package com.peterlavalle
+
+import java.io.File
+
+object Home {
+  /**
+    * This is an icky work around for sbt not packing resources into tests (AFAIK) and IDEA running sbt from a folder that's not the project folder
+    */
+  lazy val projectFolder = {
+    val initialFolder = new File(".").getAbsoluteFile.getParentFile
+
+    if (initialFolder.getName == "modules" && initialFolder.getParentFile.getName == ".idea")
+      initialFolder.getParentFile.getParentFile
+
+    else if (new File(initialFolder, "build.sbt").exists() && new File(initialFolder, "src/test/scala/com/peterlavalle/Home.scala").exists())
+      initialFolder
+
+    else {
+      val message = "Don't know where from `" + initialFolder + "`"
+      System.err.println(message)
+      sys.error(message)
+    }
+  }
+
+}
