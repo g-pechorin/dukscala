abd5abc78325eb5382805463d76b7ddc960c3983
Peter LaValle <peter.lavalle@gmail.com>
2016-03-03 22:35:55 +0000

    some methods
    --HG--
    branch : diskio

--- scad40-test/scad40-gtest.cpp
+++ scad40-test/scad40-gtest.cpp
@@ -20,6 +20,50 @@ TEST(scad40Test, duk_context)
 
 #include "D40.hpp"
 
+peterlavalle::diskio::Disk::Disk(void)
+{
+}
+
+scad40::duk_native<peterlavalle::diskio::Reading> peterlavalle::diskio::Disk::open(scad40::duk_string& path)
+{
+	auto result = peterlavalle::diskio::Reading::New(Host());
+
+	result->_path = path;
+
+	return result;
+}
+
+peterlavalle::diskio::Disk::~Disk(void)
+{
+}
+
+
+peterlavalle::diskio::Reading::Reading(void) :
+	_path(Host(), ""),
+	_handle(nullptr)
+{
+}
+
+int8_t peterlavalle::diskio::Reading::read()
+{
+	EXPECT_FALSE(true) << "STUB ; method incomplete";
+	return 0;
+}
+void peterlavalle::diskio::Reading::close()
+{
+	EXPECT_FALSE(true) << "STUB ; method incomplete";
+}
+bool peterlavalle::diskio::Reading::endOfFile()
+{
+	EXPECT_FALSE(true) << "STUB ; method incomplete";
+	return true;
+}
+
+peterlavalle::diskio::Reading::~Reading(void)
+{
+	EXPECT_FALSE(true) << "STUB ; method incomplete";
+}
+
 TEST(scad40Test, scad40)
 {
 	duk_context* ctx = duk_create_heap_default();
