912c4e706f91ecec86e3d28f37e5b08d9c6076eb
Peter LaValle <peter.lavalle@gmail.com>
2016-03-08 01:05:11 +0000

    reads a file using script-classes from native-land
    --HG--
    branch : diskio

--- scad40-test/scad40-gtest.cpp
+++ scad40-test/scad40-gtest.cpp
@@ -28,6 +28,7 @@ scad40::duk_native<peterlavalle::diskio::Reading> peterlavalle::diskio::Disk::op
 {
 	auto result = peterlavalle::diskio::Reading::New(Host());
 
+	result->_handle = fopen(path.c_str(), "r");
 	result->_path = path;
 
 	return result;
@@ -39,29 +40,53 @@ peterlavalle::diskio::Disk::~Disk(void)
 
 
 peterlavalle::diskio::Reading::Reading(void) :
-	_path(Host(), ""),
-	_handle(nullptr)
+_path(Host(), ""),
+_handle(nullptr)
 {
 }
 
 int8_t peterlavalle::diskio::Reading::read()
 {
-	EXPECT_FALSE(true) << "STUB ; method incomplete";
-	return 0;
+	assert(nullptr != _handle);
+
+	int8_t byte;
+	EXPECT_EQ(1, fread(&byte, 1, 1, _handle));
+	return byte;
 }
+
 void peterlavalle::diskio::Reading::close()
 {
-	EXPECT_FALSE(true) << "STUB ; method incomplete";
+	assert(nullptr != _handle);
+
+	fclose(_handle);
+
+	_handle = nullptr;
 }
+
 bool peterlavalle::diskio::Reading::endOfFile()
 {
-	EXPECT_FALSE(true) << "STUB ; method incomplete";
-	return true;
+	assert(nullptr != _handle);
+	auto cur = ftell(_handle);
+	if (fseek(_handle, 0, SEEK_END))
+	{
+		assert(false);
+	}
+	auto end = ftell(_handle);
+	if (fseek(_handle, cur, SEEK_SET))
+	{
+		assert(false);
+	}
+	return end == cur;
 }
 
 peterlavalle::diskio::Reading::~Reading(void)
 {
-	EXPECT_FALSE(true) << "STUB ; method incomplete";
+	if (_handle)
+	{
+		close();
+	}
+
+	assert(nullptr == _handle);
 }
 
 TEST(scad40Test, scad40)
@@ -71,6 +96,44 @@ TEST(scad40Test, scad40)
 	ASSERT_TRUE(nullptr != ctx) << "Couldn't create Duktape";
 
 	peterlavalle::diskio::install(ctx);
+
+	duk_destroy_heap(ctx);
+}
+
+TEST(scad40Test, readFileFromNative)
+{
+	duk_context* ctx = duk_create_heap_default();
+
+	ASSERT_TRUE(nullptr != ctx) << "Couldn't create Duktape";
+
+	peterlavalle::diskio::install(ctx);
+
+	const char* name = "test-temp.file";
+
+	int8_t data[] = {
+		3, 14, 19, 83
+	};
+
+	{
+		auto file = fopen(name, "w");
+		ASSERT_NE(nullptr, file) << "Failed to open file";
+		ASSERT_EQ(1, fwrite(data, sizeof(data), 1, file)) << "Failed to write data";
+		ASSERT_EQ(0, fclose(file)) << "Failed to close file";
+	}
+
+	{
+		auto reading = peterlavalle::diskio::Disk::get(ctx).open(name);
+
+		for (size_t i = 0; i < sizeof(data); ++i)
+		{
+			ASSERT_FALSE(reading->endOfFile()) << "Premature end of file @ " << i;
+
+			ASSERT_EQ(data[i], reading->read()) << "Mismatching data @ " << i;
+		}
+
+		ASSERT_TRUE(reading->endOfFile()) << "File ran-on";
+		reading->close();
+	}
 
 	duk_destroy_heap(ctx);
 }
